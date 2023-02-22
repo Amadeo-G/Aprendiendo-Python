b0841ad9d77250a279cbcda234a38756cb8d308c
Amadeo-G <amadeoraulgiannantonio@gmail.com>
2023-02-22 11:44:10 -0300

    Ejercicio 001
    Encontrar la superficie de un cuadrado sabiendo cuánto mide uno de sus lados.

---
+++ Ejercicios/Ejercicio 001.py
@@ -0,0 +1,4 @@
+lado = float(input("Para Encontrar la superficie de un cuadrado sabiendo cuánto mide uno de sus lados, ingrese el valor del mismo: "))
+superficie = lado * lado
+
+print(f"La superficie del cuadrado es {superficie}")

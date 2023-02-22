b112fb715adb256aa1c8261e924e8a54726ed2a4
Amadeo-G <amadeoraulgiannantonio@gmail.com>
2023-02-22 12:11:49 -0300

    Ejercicio 002
    Ingresar un valor entero por teclado, y calcular el cuadrado y el cubo de dicho valor

---
+++ Ejercicios/Ejercicio 002.py
@@ -0,0 +1,9 @@
+"""Ingresar un valor entero por teclado, y calcular el cuadrado y el cubo de dicho valor"""
+
+num = int(input("Ingresa un valor entero: "))
+
+cuadrado = num ** 2
+cubo = num ** 3
+
+print(f"El cuadrado de {num} es {cuadrado}")
+print(f"El cubo de {num} es {cubo}")
